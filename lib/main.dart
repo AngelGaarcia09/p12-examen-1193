240517b57ac7e4ea732a41cb1252832908b2bf62
ANGEL_MISAEL_GARCIA_HERNANDEZ <a.22308051281193@cbtis128.edu.mx>
2025-03-11 15:54:07 +0000

    otra

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,78 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: QuizScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class QuizScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Center(child: Text('Quiz Map Garcia 1193')), // Centrar el título
+        backgroundColor: Colors.blue,
+        automaticallyImplyLeading:
+            false, // Evita que el título se desplace con el botón de regreso
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Text(
+              'Which Technology is Good?',
+              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 10), // Espacio mínimo entre el texto y los botones
+            QuizButton(text: 'iOS'),
+            QuizButton(text: 'Android'),
+            QuizButton(text: 'Flutter'),
+            QuizButton(text: 'React Native'),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class QuizButton extends StatelessWidget {
+  final String text;
+  QuizButton({required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(vertical: 4.0), // Espacio mínimo entre botones
+      child: SizedBox(
+        width: double.infinity, // Todos los botones con el mismo ancho
+        height: 50, // Todos los botones con la misma altura
+        child: ElevatedButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('You selected $text')),
+            );
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          child: Text(
+            text,
+            style: TextStyle(fontSize: 18, color: Colors.white),
+          ),
         ),
       ),
     );
